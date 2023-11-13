a11015519938771ec5480fb3f74b2254efab8b64
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-11-13 20:06:03 -0300

    feat: implementing footer and author

--- php/connection.php
+++ php/connection.php
@@ -1,13 +1,12 @@
 <?php
-  ini_set("display_errors", 1);
-  error_reporting(E_ALL);  
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 
-  $hostname = "localhost";
-  $user = "root";
-  $pass = "ifsp"; 
-  $database = "eleicao";
-  $conn = mysqli_connect($hostname, $user, $pass, $database);
-  if (!$conn) {
-    die("Conexão falhou: " . mysqli_connect_error());
-  }
-?>
+$hostname = "localhost";
+$user = "root";
+$pass = "";
+$database = "eleicao";
+$conn = mysqli_connect($hostname, $user, $pass, $database);
+if (!$conn) {
+  die("Conexão falhou: " . mysqli_connect_error());
+}
